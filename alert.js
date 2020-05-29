2306e8c79633371a669fe72db24ae24a94c6384c
Carellep <63817789+Carellep@users.noreply.github.com>
2020-05-29 10:53:34 -0400

    Bonjour

---
+++ alert.js
@@ -0,0 +1,21 @@
+alert('premier site');
+console.log('Bonjour tout le monde');
+console.log('Mon premier site');
+let nom = 'Jean';
+let admin = nom;
+console.log(admin);
+
+alert(`Bonjour, ${nom}!`);
+let estplusgrang = 4>2;
+console.log(estplusgrang);
+alert(`Hello ${1}`);
+alert(`Hello ${"nom"}`);
+alert('Hello '+nom);
+
+let nbre = true;
+nbre = String(nbre);
+alert(nbre == true);
+alert(nbre == 'true');
+
+alert(0=='0');
+alert(0==='0');
